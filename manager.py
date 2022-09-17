f96d59836b42c318a6d43716c8a737e4eae9a717
ishikawa16 <shin.0116@keio.jp>
2022-09-17 19:19:49 +0900

    update: change order of functions

--- manager.py
+++ manager.py
@@ -19,55 +19,17 @@ class Manager:
         self.rule = Rule()
 
     # プレイヤー関連の操作
-    def has_player(self, name):
-        return name in self.player_db
-
     def get_player(self, name):
         return self.player_db[name]
+
+    def has_player(self, name):
+        return name in self.player_db
 
     def add_player(self, name):
         self.player_db[name] = Player()
 
     def remove_player(self, name):
         del self.player_db[name]
-
-    def split_players(self, names):
-        team_option = self.rule.get_team_option()
-        if team_option == "-w":
-            diff = float("inf")
-            for comb in combinations(names, len(names) // 2):
-                team1_wp, team2_wp = 0, 0
-                for name, player in self.player_db.items():
-                    wp = player.get_wp()
-                    if name in comb:
-                        team1_wp += wp
-                    else:
-                        team2_wp += wp
-
-                if abs(team1_wp - team2_wp) < diff:
-                    diff = abs(team1_wp - team2_wp)
-                    team1, team2 = comb, tuple(set(names) - set(comb))
-
-        elif team_option == "-r":
-            comb = random.sample(names, len(names) // 2)
-            team1, team2 = comb, list(set(names) - set(comb))
-
-        else:
-            battle = self.battle_db[-1]
-            team1, team2 = battle.get_team1(), battle.get_team2()
-
-        return team1, team2
-
-    def get_active_players(self):
-        return [name for name, player in self.player_db.items() if player.is_active()]
-
-    def is_short(self):
-        active_players = self.get_active_players()
-        return len(active_players) < 2
-
-    def is_over(self):
-        active_players = self.get_active_players()
-        return len(active_players) > 8
 
     def display_players(self):
         msg = ""
@@ -102,18 +64,54 @@ class Manager:
                     msg += f"- {name} ( - )\n"
         return msg
 
-    # 武器関連の操作
-    def specify_weapons(self, names):
-        player2weapon = dict()
-        weapon_option = self.rule.get_weapon_option()
-        for name in names:
-            if weapon_option == "-a":
-                player2weapon[name] = None
-            else:
-                player2weapon[name] = random.choice(WEAPON_LIST)
-        return player2weapon
+    def get_active_players(self):
+        return [name for name, player in self.player_db.items() if player.is_active()]
+
+    def split_players(self, names):
+        team_option = self.rule.get_team_option()
+        if team_option == "-w":
+            diff = float("inf")
+            for comb in combinations(names, len(names) // 2):
+                team1_wp, team2_wp = 0, 0
+                for name, player in self.player_db.items():
+                    wp = player.get_wp()
+                    if name in comb:
+                        team1_wp += wp
+                    else:
+                        team2_wp += wp
+
+                if abs(team1_wp - team2_wp) < diff:
+                    diff = abs(team1_wp - team2_wp)
+                    team1, team2 = comb, tuple(set(names) - set(comb))
+
+        elif team_option == "-r":
+            comb = random.sample(names, len(names) // 2)
+            team1, team2 = comb, list(set(names) - set(comb))
+
+        else:
+            battle = self.battle_db[-1]
+            team1, team2 = battle.get_team1(), battle.get_team2()
+
+        return team1, team2
+
+    def is_short(self):
+        active_players = self.get_active_players()
+        return len(active_players) < 2
+
+    def is_over(self):
+        active_players = self.get_active_players()
+        return len(active_players) > 8
 
     # バトル関連の操作
+    def get_battle(self):
+        return self.battle
+
+    def get_latest_battle(self):
+        if len(self.battle_db) == 0:
+            return None
+        else:
+            return self.battle_db[-1]
+
     def prepare_battle(self):
         active_players = self.get_active_players()
         team1, team2 = self.split_players(active_players)
@@ -129,14 +127,15 @@ class Manager:
         self.battle_db.append(latest_battle)
         self.battle = None
 
-    def get_battle(self):
-        return self.battle
-
-    def get_latest_battle(self):
-        if len(self.battle_db) == 0:
-            return None
-        else:
-            return self.battle_db[-1]
+    def specify_weapons(self, names):
+        weapon_option = self.rule.get_weapon_option()
+        player2weapon = dict()
+        for name in names:
+            if weapon_option == "-a":
+                player2weapon[name] = None
+            else:
+                player2weapon[name] = random.choice(WEAPON_LIST)
+        return player2weapon
 
     def calculate_wp(self, winner):
         team1 = self.battle.get_team1()
